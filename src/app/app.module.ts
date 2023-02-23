3bc99ad8903073f7391db71b5189a702bc879f63
Santosh Turerao INFX04882 <INFX04882@TANDON.LOCAL>
2023-02-23 19:11:20 +0530

    updated structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/components/header/header.component';
+import { FooterComponent } from './layout/components/footer/footer.component';
+import { HomeComponent } from './layout/pages/home/home.component';
+import { AdminComponent } from './layout/pages/admin/admin.component';
+import { UserComponent } from './layout/pages/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AdminComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
